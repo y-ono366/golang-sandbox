ceeaca5722dd92daa5bfd8ad51ce9acd2edc2ba4
yusuke ohno <y.ono.varial@gmail.com>
2018-11-15 00:18:03 +0900

    fix next go

--- date/next.go
+++ date/next.go
@@ -6,23 +6,15 @@ import(
 )
 
 func main() {
-	pocky := time.Date(2018,11,11,0,0,0,0,time.Local)
+	pocky := time.Date(2018,10,15,0,0,0,0,time.Local)
 	nowdate :=time.Now()
 
 	var pockylist []string
-
 	for nowdate.Unix() > pocky.Unix() {
 		pockylist = append(pockylist,pocky.Format("2006/1/2"))
 		pocky = pocky.AddDate(0, 0, 28-1)
 	}
 	pockylist = append(pockylist,pocky.Format("2006/1/2"))
 
-	fmt.Println(pockylist)
+	fmt.Printf("before  "+ pockylist[len(pockylist)-2] + " next  "+pockylist[len(pockylist)-1])
 }
-
-
-
-
-
-
-
